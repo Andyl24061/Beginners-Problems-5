f46178f3d5321caa65294e128c47aad18f8287c9
Andy <161436408+Andyl24061@users.noreply.github.com>
2024-05-20 20:23:00 +1200

    Create Starter.py

---
+++ Starter.py
@@ -0,0 +1,4 @@
+numsList = [7, 6, 23, 8.18, 18, 8, 7.2, 85, 915, 12]
+print("The biggest number is ",max(numsList))
+print("The smallest number is ",min(numsList))
+print("The average is ",sum(numsList)/len(numsList))
